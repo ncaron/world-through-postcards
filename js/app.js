b2a7b4be8d1e99b63bef40d373eb72483447fdb4
Niko <niko.caron90@gmail.com>
2017-08-21 15:27:46 -0300

    Navigation between pages
    Set up an event listener to determine if a country is clicked and if it
    is, build that page with the country data(not yet complete) and add it
    to a list of visited pages so the HTML will not need to be built again
    if the user revisits the same page in a single session. Also goes back
    to the home page on Home or logo click. Added a variable to determine
    which page the user is currently on so there will not be a refresh if
    Home or logo is clicked while on the home page.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,21 @@ var App = {
   data: data,
   keys: Object.keys(this.data),
   mainContent: '',
+  countryPages: {},
+  currentPage: 'home',
   cacheElements: function() {
     this.content = document.getElementById('content');
+    this.home = document.getElementById('home');
+    this.about = document.getElementById('about');
   },
   cacheTemplates: function() {
     this.countryTemplate = document.getElementById('country-template').innerHTML.trim();
+    this.countryPageTemplate = document.getElementById('country-page-template').innerHTML.trim();
   },
   replaceCountryHTML: function(countryTemplate, country) {
-    countryTemplate = countryTemplate.replace(/{{COUNTRY}}/g, data[country].name);
-    countryTemplate = countryTemplate.replace(/{{CODE}}/g, data[country].code);
+    countryTemplate = countryTemplate.replace(/{{COUNTRY-DATA}}/g, country);
+    countryTemplate = countryTemplate.replace(/{{COUNTRY}}/g, this.data[country].name);
+    countryTemplate = countryTemplate.replace(/{{CODE}}/g, this.data[country].code);
 
     return countryTemplate;
   },
@@ -22,9 +28,65 @@ var App = {
   renderCountries: function() {
     this.content.innerHTML = this.mainContent;
   },
+  getClickedCountry: function(el) {
+    if (el && !el.matches('.country')) {
+      return this.getClickedCountry(el.parentElement);
+    } else {
+      return el;
+    }
+  },
+  handleClickedCountry: function(countryClicked) {
+    this.currentPage = countryClicked.dataset.country;
+
+    this.buildCountryPage();
+    this.renderCountryPage();
+    this.removeActiveClass();
+  },
+  buildCountryPage: function() {
+    if (!this.countryPages[this.currentPage]) {
+      this.countryPages[this.currentPage] = this.replaceCountryPageHTML(this.countryPageTemplate, this.currentPage);
+    }
+  },
+  replaceCountryPageHTML: function(countryPageTemplate, country) {
+    countryPageTemplate = countryPageTemplate.replace(/{{COUNTRY}}/g, this.data[country].name);
+
+    return countryPageTemplate;
+  },
+  renderCountryPage: function() {
+    this.content.innerHTML = this.countryPages[this.currentPage];
+  },
+  goBackHome: function(target) {
+    if (this.currentPage !== 'home') {
+      this.renderCountries();
+      this.addActiveClass(target);
+      this.currentPage = 'home';
+    }
+  },
+  removeActiveClass: function() {
+    this.home.classList.remove('active');
+    this.about.classList.remove('active');
+  },
+  addActiveClass: function(target) {
+    target.classList.add('active');
+  },
+  bindEvents: function() {
+    document.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      var target = e.target;
+      var countryClicked = this.getClickedCountry(target);
+
+      if (countryClicked) {
+        this.handleClickedCountry(countryClicked);
+      } else if (target.id === 'home' || target.id === 'title') {
+        this.goBackHome(this.home);
+      }
+    }.bind(this));
+  },
   init: function() {
     this.cacheElements();
     this.cacheTemplates();
+    this.bindEvents();
     this.buildHomePage();
     this.renderCountries();
   }
