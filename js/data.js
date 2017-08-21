39b1b70ccde72ad0a84889446786b2cc52104839
Niko <niko.caron90@gmail.com>
2017-08-21 00:48:44 -0300

    Data Struture
    Added the data structure for the data but it may change in the future,
    also added the images for the cards added in the data.
    Changed the eslint config to allow trailing commas.

---
+++ js/data.js
@@ -0,0 +1,189 @@
+var data = {
+  algeria: {
+    name: 'Algeria',
+    code: 'dz',
+    cards: [
+      {
+        front: './assets/cards/front/front003.jpg',
+        back: './assets/cards/back/back003.jpg',
+      },
+    ],
+  },
+
+  australia: {
+    name: 'Australia',
+    code: 'au',
+    cards: [
+      {
+        front: './assets/cards/front/front018.jpg',
+        back: './assets/cards/back/back018.jpg',
+      },
+    ],
+  },
+
+  canada: {
+    name: 'Canada',
+    code: 'ca',
+    cards: [
+      {
+        front: './assets/cards/front/front001.jpg',
+        back: './assets/cards/back/back001.jpg',
+      },
+    ],
+  },
+
+  china: {
+    name: 'China',
+    code: 'cn',
+    cards: [
+      {
+        front: './assets/cards/front/front014.jpg',
+        back: './assets/cards/back/back014.jpg',
+      },
+    ],
+  },
+
+  croatia: {
+    name: 'Croatia',
+    code: 'hr',
+    cards: [
+      {
+        front: './assets/cards/front/front004.jpg',
+        back: './assets/cards/back/back004.jpg',
+      },
+      {
+        front: './assets/cards/front/front005.jpg',
+        back: './assets/cards/back/back005.jpg',
+      },
+    ],
+  },
+
+  estonia: {
+    name: 'Estonia',
+    code: 'ee',
+    cards: [
+      {
+        front: './assets/cards/front/front019.jpg',
+        back: './assets/cards/back/back019.jpg',
+      },
+    ],
+  },
+
+  finland: {
+    name: 'Finland',
+    code: 'fi',
+    cards: [
+      {
+        front: './assets/cards/front/front002.jpg',
+        back: './assets/cards/back/back002.jpg',
+      },
+      {
+        front: './assets/cards/front/front008.jpg',
+        back: './assets/cards/back/back008.jpg',
+      },
+    ],
+  },
+
+  germany: {
+    name: 'Germany',
+    code: 'de',
+    cards: [
+      {
+        front: './assets/cards/front/front016.jpg',
+        back: './assets/cards/back/back016.jpg',
+      },
+    ],
+  },
+
+  greece: {
+    name: 'Greece',
+    code: 'gr',
+    cards: [
+      {
+        front: './assets/cards/front/front017.jpg',
+        back: './assets/cards/back/back017.jpg',
+      },
+    ],
+  },
+
+  malaysia: {
+    name: 'Malaysia',
+    code: 'my',
+    cards: [
+      {
+        front: './assets/cards/front/front013.jpg',
+        back: './assets/cards/back/back013.jpg',
+      },
+    ],
+  },
+
+  netherlands: {
+    name: 'Netherlands',
+    code: 'nl',
+    cards: [
+      {
+        front: './assets/cards/front/front007.jpg',
+        back: './assets/cards/back/back007.jpg',
+      },
+      {
+        front: './assets/cards/front/front012.jpg',
+        back: './assets/cards/back/back012.jpg',
+      },
+    ],
+  },
+
+  philippines: {
+    name: 'Philippines',
+    code: 'ph',
+    cards: [
+      {
+        front: './assets/cards/front/front009.jpg',
+        back: './assets/cards/back/back009.jpg',
+      },
+    ],
+  },
+
+  russia: {
+    name: 'Russia',
+    code: 'ru',
+    cards: [
+      {
+        front: './assets/cards/front/front011.jpg',
+        back: './assets/cards/back/back011.jpg',
+      },
+    ],
+  },
+
+  taiwan: {
+    name: 'Taiwan',
+    code: 'tw',
+    cards: [
+      {
+        front: './assets/cards/front/front006.jpg',
+        back: './assets/cards/back/back006.jpg',
+      },
+    ],
+  },
+
+  turkey: {
+    name: 'Turkey',
+    code: 'tr',
+    cards: [
+      {
+        front: './assets/cards/front/front010.jpg',
+        back: './assets/cards/back/back010.jpg',
+      },
+    ],
+  },
+
+  unitedKingdom: {
+    name: 'United Kingdom',
+    code: 'gb',
+    cards: [
+      {
+        front: './assets/cards/front/front015.jpg',
+        back: './assets/cards/back/back015.jpg',
+      },
+    ],
+  },
+};
