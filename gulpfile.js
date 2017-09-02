ef5dbbab1c6c3a8460d6e2743e0c3fcf5bac982a
Niko <niko.caron90@gmail.com>
2017-09-01 23:56:20 -0300

    Minifying Files

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,18 @@ var connect = require('gulp-connect');
 var open = require('gulp-open');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var minHTML = require('gulp-htmlmin');
+var minCSS = require('gulp-clean-css');
+var minJS = require('gulp-uglify');
 
 var config = {
   port: 9005,
   devBaseUrl: 'http://localhost',
   paths: {
-    html: './*.html',
-    css: './css/*',
-    js: './js/*.js',
+    html: ['index.html'],
+    css: ['./css/reset.css', './css/style.scss'],
+    js: ['./js/data.js', './js/app.js'],
     assets: './assets/**/**/*',
     dist: './dist',
   }
@@ -32,20 +36,25 @@ gulp.task('open', ['connect'], function() {
 
 gulp.task('html', function() {
   gulp.src(config.paths.html)
+      .pipe(minHTML({ collapseWhitespace: true, processScripts: ['text/template']}))
       .pipe(gulp.dest(config.paths.dist))
       .pipe(connect.reload());
 });
 
 gulp.task('css', function() {
   return gulp.src(config.paths.css)
+             .pipe(concat('style.css'))
              .pipe(sass().on('error', sass.logError))
+             .pipe(minCSS())
              .pipe(gulp.dest(config.paths.dist + '/css'))
              .pipe(connect.reload());
 });
 
 gulp.task('js', function() {
   gulp.src(config.paths.js)
+      .pipe(concat('app.js'))
       .pipe(babel({ presets: ['es2015'] }))
+      .pipe(minJS())
       .pipe(gulp.dest(config.paths.dist + '/js'))
       .pipe(connect.reload());
 });
