8ad83cb6aa9f0e5e45f2df1adeefa09780c4dfaf
Niko <niko.caron90@gmail.com>
2017-09-08 04:34:07 -0300

    Changed about page layout
    Map now appears after the about text and before the built with list

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -3,15 +3,19 @@ import PostcardMap from '../Map/Map';
 import './AboutPage.css';
 
 export default ({data}) =>
-  <div>
-    <div className="about">
+  <div className="about">
+    <div className="about-text">
       <p>Hello World,
       <br />
       <br />
       I'm Niko. I currently live in Canada and I collect postcards from all over the world. I'm a new software developer and decided to build this website as practice so I can show my collection to the world!
       </p>
       <p>I have over 200 cards so stay tuned as I add more to this site.</p>
+    </div>
 
+    <PostcardMap data={data} />
+
+    <div className="built-with">
       <p>Built with:</p>
       <ul>
         <li><a href="https://facebook.github.io/react/" target="_blank" rel="noopener noreferrer">React</a></li>
@@ -20,5 +24,4 @@ export default ({data}) =>
         <li><a href="http://flag-icon-css.lip.is/" target="_blank" rel="noopener noreferrer">flag-icon-css</a></li>
       </ul>
     </div>
-    <PostcardMap data={data} />
   </div>
