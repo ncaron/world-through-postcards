a761fc4ca57455e0954adffc924949921baaee94
Niko <niko.caron90@gmail.com>
2017-09-08 01:48:29 -0300

    BrowserRouter -> HashRouter fix for #2
    Changed to HashRouter to possibly fix issue #2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import MainPage from './components/MainPage/MainPage';
 import Footer from './components/Footer/Footer';
