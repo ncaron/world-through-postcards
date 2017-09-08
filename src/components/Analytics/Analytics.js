94979283374b8c574888144fabdeda4f38ae9b70
Niko <niko.caron90@gmail.com>
2017-09-08 15:39:54 -0300

    Google Analytics Fix
    No longer removing the hash from the url to add to Google Analytics
    because clicking on the page in the analytics console gave a 404 error.

--- src/components/Analytics/Analytics.js
+++ src/components/Analytics/Analytics.js
@@ -3,7 +3,7 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('UA-106127649-1');
 
 export default () => {
-  const path = window.location.hash.replace('#', '');
+  const path = window.location.hash;
 
   ReactGA.set({ page: path });
   ReactGA.pageview(path);
